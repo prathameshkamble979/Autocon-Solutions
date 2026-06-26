de5aaada64af0141def0b9cc30cd574748d89d96
Prathamesh Kamble <prathameshdk06@gmail.com>
2026-06-26 11:50:33 +0530

    chore: remove test file

--- server/test_live.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-axios.post('https://autocon-server.onrender.com/api/ai/search', {
-    query: "heavy belt conveyor",
-    language: "en-US"
-}).then(res => console.log("SUCCESS:", res.data))
-  .catch(err => console.log("ERROR:", err.response ? err.response.data : err.message));
